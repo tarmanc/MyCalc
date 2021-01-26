740421f73905807d50fe86ca457a708c267f086c
adyingwish <62962574+adyingwish@users.noreply.github.com>
2021-01-27 00:29:55 +0300

    Implements Divide Function

--- app/src/main/java/com/example/mycalc/DataOperation.java
+++ app/src/main/java/com/example/mycalc/DataOperation.java
@@ -2,34 +2,38 @@ package com.example.mycalc;
 
 import org.jetbrains.annotations.NotNull;
 
-enum operator{ADD, SUBTRACT, MULTIPLY, DIVIDE, BLANK }
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+enum operator {ADD, SUBTRACT, MULTIPLY, DIVIDE, BLANK, PERCENTAGE}
+
 class DataOperation {
 
     //Variables which helps to decide what operation is to be performed
-    private int operatorCount=0;
+    private int operatorCount = 0;
 
-    private StringBuilder firstVal=new StringBuilder("0");
-    private StringBuilder secondVal=new StringBuilder();
-    private boolean enter=false;
-    StringBuilder input=new StringBuilder();
-    StringBuilder answer=new StringBuilder();
-    private float result=0;
+    private StringBuilder firstVal = new StringBuilder("0");
+    private StringBuilder secondVal = new StringBuilder();
+    private boolean enter = false;
+    StringBuilder input = new StringBuilder();
+    StringBuilder answer = new StringBuilder();
+    private float result = 0;
 
 
     //Getting string values in input and answer TextView
-    private String getStringFromView(@NotNull StringBuilder viewString){
+    private String getStringFromView(@NotNull StringBuilder viewString) {
         return viewString.toString();
     }
 
     //Setting String values from input and answer from MainActivity
-    void setStringFromView(String inputString, String answerString){
-        input.replace(0,input.length(),inputString);
-        answer.replace(0,answer.length(),answerString);
+    void setStringFromView(String inputString, String answerString) {
+        input.replace(0, input.length(), inputString);
+        answer.replace(0, answer.length(), answerString);
     }
 
     //Operation based on operator:
-    private float operation(operator op, StringBuilder firstVal, StringBuilder secondVal){
-        try{
+    private float operation(operator op, StringBuilder firstVal, StringBuilder secondVal) {
+        try {
             switch (op) {
                 //returns evaluation based on the operator button clicked by user
                 case ADD:
@@ -39,21 +43,29 @@ class DataOperation {
                 case MULTIPLY:
                     return (Float.parseFloat(firstVal.toString()) * Float.parseFloat(secondVal.toString()));
                 case DIVIDE:
-                    return (Float.parseFloat(firstVal.toString())/Float.parseFloat(secondVal.toString()));
+
+                    //Divide Operation with Float is faulty. See details: https://floating-point-gui.de/basic/
+                    BigDecimal firstValue = new BigDecimal(firstVal.toString());
+                    BigDecimal secondValue = new BigDecimal(secondVal.toString());
+
+                    //Precision indicates total showing digits
+                    BigDecimal result = firstValue.divide(secondValue, new MathContext(10));
+
+                    return result.floatValue();
+                case PERCENTAGE:
                 default:
                     //If only firstVal is present then returns firstVal
                     return Float.parseFloat(firstVal.toString());
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             //Even in case of any exceptions returns firstVal as its default value is 0
             return Float.parseFloat(firstVal.toString());
         }
 
     }
 
-    void arithmeticOperator(@org.jetbrains.annotations.NotNull operator op){
-        String sign ;
+    void arithmeticOperator(@org.jetbrains.annotations.NotNull operator op) {
+        String sign;
         switch (op) {
             case ADD:
                 sign = "\u002b";
@@ -72,80 +84,78 @@ class DataOperation {
         }
 
 
-        if(getStringFromView(input).matches(".*[\\u002b\\u2212\\u00d7\\u00f7]$")) {
+        if (getStringFromView(input).matches(".*[\\u002b\\u2212\\u00d7\\u00f7]$")) {
 
-            int length=getStringFromView(input).length();
-            input.replace(0,input.length(),String.format("%s%s", getStringFromView(input).substring(0, length - 1),sign));
+            int length = getStringFromView(input).length();
+            input.replace(0, input.length(), String.format("%s%s", getStringFromView(input).substring(0, length - 1), sign));
 
-        }
-        else {
+        } else {
 
-            if (!getStringFromView(input).equals("")){
-                input.replace(0,input.length(),String.format("%s%s", getStringFromView(input), sign));
-            }
-            else{
-                input.replace(0,input.length(),String.format("0%s", sign));
+            if (!getStringFromView(input).equals("")) {
+                input.replace(0, input.length(), String.format("%s%s", getStringFromView(input), sign));
+            } else {
+                input.replace(0, input.length(), String.format("0%s", sign));
             }
             secondVal.setLength(0);
-            if (operatorCount!=0){
+            if (operatorCount != 0) {
                 firstVal.setLength(0);
                 firstVal.append(result);
             }
-            operatorCount+=1;
+            operatorCount += 1;
         }
     }
 
     void clear() {
         input.setLength(0);
-        answer.setLength(0);answer.append("0");
-        MainActivity.op=operator.BLANK;
-        firstVal.setLength(0);firstVal.append("0");
+        answer.setLength(0);
+        answer.append("0");
+        MainActivity.op = operator.BLANK;
+        firstVal.setLength(0);
+        firstVal.append("0");
         secondVal.setLength(0);
-        operatorCount=0;
-        enter=false;
-        result=0;
+        operatorCount = 0;
+        enter = false;
+        result = 0;
     }
 
     void delete() {
 
         String str = getStringFromView(input); //take input value from TextView
         //if length of input greater than 1 than its go in if condition
-        if (str.length() >1 ) {
+        if (str.length() > 1) {
             str = str.substring(0, str.length() - 1); // remove last 1 element from str
             input = new StringBuilder(str);                   //and set it into input
             // Log.i(str + "     ","");
-            if(!secondVal.toString().equals("")){       //secondValue is not null
+            if (!secondVal.toString().equals("")) {       //secondValue is not null
                 String value = secondVal.toString();
                 value = value.substring(0, value.length() - 1); // remove last 1 element from second value
                 secondVal = new StringBuilder(value);
-            }else if(!MainActivity.op.equals(operator.BLANK)){    // operator is not null
+            } else if (!MainActivity.op.equals(operator.BLANK)) {    // operator is not null
                 MainActivity.op = operator.BLANK;
-            }else {
+            } else {
                 String value = firstVal.toString();
                 value = value.substring(0, value.length() - 1);  // remove last 1 element from first value
                 firstVal = new StringBuilder(value);
             }
-                result=operation(MainActivity.op,firstVal,secondVal);    // perform operation and result
-                answer = new StringBuilder("=" + Float.toString(result));     // set result in anwer TextView
-            }
-        else if (str.length() <=1 ) {
+            result = operation(MainActivity.op, firstVal, secondVal);    // perform operation and result
+            answer = new StringBuilder("=" + Float.toString(result));     // set result in anwer TextView
+        } else if (str.length() <= 1) {
             input = new StringBuilder("0");
             answer = new StringBuilder("0");
         }
     }
 
-    void equals(){
-        if (enter){
+    void equals() {
+        if (enter) {
             //If user already clicks enter and did not entered anything then do nothing
-        }
-        else{
-            result = operation(MainActivity.op,firstVal,secondVal);
+        } else {
+            result = operation(MainActivity.op, firstVal, secondVal);
             answer.setLength(0);
             answer.append(String.format("=%s", result));
-            if (!getStringFromView(input).matches(".*[+\\-*/%]$") ) {
+            if (!getStringFromView(input).matches(".*[+\\-*/%]$")) {
                 //If both values anr given with operator in between then reset operartor and set enter
-                MainActivity.op=operator.BLANK;
-                enter=true;
+                MainActivity.op = operator.BLANK;
+                enter = true;
                 //result=0;
             }
 
@@ -154,62 +164,57 @@ class DataOperation {
     }
 
     //Setter function to set value 0,1,2...based on respective button
-    void setValue(String val){
+    void setValue(String val) {
 
 
         //When operator is set, user clicked value is entered in secondVal variable
-        operator op=MainActivity.getOperator();
-        if(op!= operator.BLANK){
+        operator op = MainActivity.getOperator();
+        if (op != operator.BLANK) {
             //Handle point
-            if((secondVal.indexOf(".")>-1) && val.equals(".")) {
+            if ((secondVal.indexOf(".") > -1) && val.equals(".")) {
 
-            }
-            else{
+            } else {
 
                 //If variable is equal to zero, replace it by clicked value or concat in it
                 if (!secondVal.toString().equals("0")) {
                     secondVal.append(val);
-                    input.replace(0,input.length(),String.format("%s%s", getStringFromView(input), val));
-                }
-                else{
+                    input.replace(0, input.length(), String.format("%s%s", getStringFromView(input), val));
+                } else {
                     secondVal.setLength(0);
                     secondVal.append(val);
-                    int length=getStringFromView(input).length();
-                    input.replace(0,input.length(),String.format("%s%s", getStringFromView(input).substring(0, length - 1), val));
+                    int length = getStringFromView(input).length();
+                    input.replace(0, input.length(), String.format("%s%s", getStringFromView(input).substring(0, length - 1), val));
                 }
                 //Also evaluate result as the user clicks a value and display it
-                result=operation(MainActivity.op,firstVal,secondVal);
-                answer.replace(0,answer.length(),String.format("=%s", result));
+                result = operation(MainActivity.op, firstVal, secondVal);
+                answer.replace(0, answer.length(), String.format("=%s", result));
             }
         }
         //Else value is added in firstVal variable
         else {
-            if (enter){
+            if (enter) {
                 //If enter is clicked set all value to default
                 clear();
             }
-            if((firstVal.indexOf(".")>-1) && val.equals(".")){
-            }
-            else{
+            if ((firstVal.indexOf(".") > -1) && val.equals(".")) {
+            } else {
 
                 if (!firstVal.toString().equals("0")) {
                     firstVal.append(val);
-                    input.replace(0,input.length(),String.format("%s%s", getStringFromView(input), val));
-                }
-                else{
+                    input.replace(0, input.length(), String.format("%s%s", getStringFromView(input), val));
+                } else {
                     firstVal.setLength(0);
                     firstVal.append(val);
-                    int length=getStringFromView(input).length();
+                    int length = getStringFromView(input).length();
                     //Condition to differentiate between first entere value and already having string in input
-                    if(length>0){
-                        input.replace(0,input.length(),String.format("%s%s", getStringFromView(input).substring(0, length - 1), val));
-                    }
-                    else{
-                        input.replace(0,input.length(),val);
+                    if (length > 0) {
+                        input.replace(0, input.length(), String.format("%s%s", getStringFromView(input).substring(0, length - 1), val));
+                    } else {
+                        input.replace(0, input.length(), val);
                     }
                 }
                 //Set variable value on display
-                answer.replace(0,answer.length(),String.format("=%s", firstVal.toString()));
+                answer.replace(0, answer.length(), String.format("=%s", firstVal.toString()));
                 enter = false;
             }
         }
